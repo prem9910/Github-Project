0246f608d05a6dd346a27f381d302cce6ef05e38
Prèm Prakash <103106787+prem9910@users.noreply.github.com>
2023-02-06 09:58:01 +0530

    used bootstrap
    Signed-off-by: Prèm Prakash <103106787+prem9910@users.noreply.github.com>

--- React Course/my-app/src/App.js
+++ React Course/my-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Prem Prakash
-        </a>
-      </header>
-    </div>
+    <>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a href="/" class="navbar-brand">My First App</a>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    
+    </>
   );
 }
 
